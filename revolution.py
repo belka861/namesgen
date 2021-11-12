5a45411aa8b84182a5d634b534f5ef19e8196b7b
belka861 <83434319+belka861@users.noreply.github.com>
2021-11-11 18:36:31 -0800

    Create revolution.py

---
+++ revolution.py
@@ -0,0 +1,1 @@
+#curl -v -F _wpcf7_version=5.4.2 -F _wpcf7_locale=ru_RU -F _wpcf7_unit_tag=wpcf7-f1319-p2584-o1 -F _wpcf7_container_post=2584 -F 'your-name=Валентина Васильева' -F your-email=g56546hgh@yandex.ru -F 'your-message=не получается пройти регистрацию 2'  https://revolutexpert.co/ru/wp-json/contact-form-7/v1/contact-forms/1319/feedback
